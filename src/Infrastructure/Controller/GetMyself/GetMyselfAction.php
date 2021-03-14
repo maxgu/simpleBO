b57fc5bc5d57c20524895eb9f4177c28c7bc2950
max.gulturyan <max.gulturyan@devellar.com>
2021-03-14 13:00:10 +0200

    add /api/get-myself

---
+++ src/Infrastructure/Controller/GetMyself/GetMyselfAction.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Infrastructure\Controller\GetMyself;
+
+use Infrastructure\Controller\AbstractAction;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+
+class GetMyselfAction extends AbstractAction
+{
+    #[Route('/api/get-myself', name: 'auth', methods: ["GET","HEAD"])]
+    public function handle(): JsonResponse
+    {
+        return $this->respondSuccess([
+            'userName' => 'admin',
+            'name' => 'John Doe',
+            'email' => 'john.doe@gmail.com',
+            'role' => 'Admin',
+        ]);
+    }
+}
